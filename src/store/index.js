ace85cfe034cbbb3bbf2d4f0651be0eb1631a6cc
fredgoum <alfredgoumou@gmail.com>
2020-09-06 13:59:18 +0200

    storage favorite beers in localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,21 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const favoriteBeersSaved = localStorage.getItem('favoriteBeers');
+
 export default new Vuex.Store({
+  namespaced: true,
   state: {
-    favoriteBeers: [],
+    favoriteBeers: (favoriteBeersSaved) ? JSON.parse(favoriteBeersSaved) : [],
   },
   mutations: {
-    updateFavoriteBeers(state, newFavoriteBeers) {
-      state.favoriteBeers = newFavoriteBeers;
+    updateFavoriteBeers(state, favoriteBeers) {
+      if (window.localStorage) { // if window localStorage is available, save favorite beers ids
+        localStorage.setItem('favoriteBeers', JSON.stringify(favoriteBeers));
+      } else {
+        console.log('localStorage is not available');
+        state.favoriteBeers = favoriteBeers;
+      }
     },
   },
   actions: {
